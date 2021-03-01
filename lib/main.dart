bc3d5ffbe62b3558bc08df68f1f82c160d17f3a1
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2021-02-28 22:22:04 -0300

    chore: create flutter app

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Center(
+        child: Text('Hello, Flutter!'),
+      ),
+    );
+  }
+}
