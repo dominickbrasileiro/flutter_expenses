5c7ff01372aa01fe0a1e9b6e1be6116af0513de2
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2021-02-28 22:43:51 -0300

    feat: add mocked transactions

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_expenses/models/transaction.dart';
 
 main() => runApp(ExpensesApp());
 
@@ -12,6 +13,21 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> _transactions = [
+    Transaction(
+      id: 1,
+      title: 'Food',
+      value: 11076,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 2,
+      title: 'A New Smartphone',
+      value: 46000,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,9 +41,13 @@ class MyHomePage extends StatelessWidget {
           Card(
             child: Text('Graph'),
           ),
-          Card(
-            child: Text('Transactions List'),
-          )
+          Column(
+            children: _transactions.map((transaction) {
+              return Card(
+                child: Text(transaction.title),
+              );
+            }).toList(),
+          ),
         ],
       ),
     );
